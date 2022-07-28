a93f9a2105a05feb127ca0774d07c65a782e42a4
maulanwalker <maulana.hasanudin.tif18@gmail.com>
2022-07-28 23:52:16 +0700

    modifying buttons using a component approach

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -15,31 +15,54 @@ const TodoItem = ({todo, index}) => {
                 <div className="block w-2/6">
                     { !todo.isCompleted ?
                     <div>
-                        <button className="py-1 px-4 bg-green-600 rounded text-white" onClick={() => toggleTodo(index)} >
+                        <button className="btn-act bg-green-600" onClick={() => toggleTodo(index)} >
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 inline" viewBox="0 0 20 20" fill="currentColor">
+        <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+        </svg>
                             Completed
                         </button> 
-                        <button className="py-1 px-4 bg-sky-600 rounded text-white" onClick={() => getTodoForUpdate(index)} >
+                        <button className="btn-act bg-sky-600" onClick={() => getTodoForUpdate(index)} >
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 inline" viewBox="0 0 20 20" fill="currentColor">
+  <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+  <path fillRule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clipRule="evenodd" />
+</svg>
                             Update
                         </button> 
-                        <button className="py-1 px-4 bg-rose-400 rounded text-white" disabled onClick={() => deleteTodo(index)} >
+                        <button className="btn-act bg-rose-200 cursor-not-allowed" disabled onClick={() => deleteTodo(index)} >
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 inline" viewBox="0 0 20 20" fill="currentColor">
+  <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+</svg>
                             Delete
                         </button>
                     </div>:
                     <div>
-                        <button className="py-1 px-4 bg-green-600 rounded text-white" onClick={() => toggleTodo(index)} >
+                        <button className="btn-act bg-green-600" onClick={() => toggleTodo(index)} >
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 inline" viewBox="0 0 20 20" fill="currentColor">
+  <path fillRule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clipRule="evenodd" />
+</svg>
                             Undo
                         </button>
-                        <button className="py-1 px-4 bg-sky-400 rounded text-white" disabled onClick={() => getTodoForUpdate(index)} >
+                        <button className="btn-act bg-sky-200 cursor-not-allowed" disabled onClick={() => getTodoForUpdate(index)} >
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 inline" viewBox="0 0 20 20" fill="currentColor">
+  <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+  <path fillRule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clipRule="evenodd" />
+</svg>
                             Update
                         </button>
-                        <button className="py-1 px-4 bg-rose-600 rounded text-white" onClick={() => deleteTodo(index)} >
+                        <button className="btn-act bg-rose-600" onClick={() => deleteTodo(index)} >
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 inline" viewBox="0 0 20 20" fill="currentColor">
+  <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+</svg>
                             Delete
                         </button>
                     </div>
                     }
                 </div>
                 :
-                <button className="p-1 bg-slate-400 rounded text-white" onClick={() => cancelUpdate()} >
+                <button className="btn-act bg-slate-400" onClick={() => cancelUpdate()} >
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 inline" viewBox="0 0 20 20" fill="currentColor">
+  <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+</svg>
                     Cancel
                 </button>
             }
